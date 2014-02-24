2b448d7af2019eb743e5595ff73baa31ac04eae6
M. C. McGrath <shidash@shidash.com>
2014-02-23 18:59:53 -0800

    Added support for fields with arrays where each item should be a separate node

--- jsontonetworkgraph.gemspec
+++ jsontonetworkgraph.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'jsontonetworkgraph'
-  s.version     = '0.0.2'
-  s.date        = '2014-02-21'
+  s.version     = '0.0.3'
+  s.date        = '2014-02-23'
   s.summary     = "Turn JSONs into network graphs"
   s.description = "Generates node and link data from any JSON."
   s.authors     = ["M. C. McGrath"]
--- lib/jsontonetworkgraph.rb
+++ lib/jsontonetworkgraph.rb
@@ -16,39 +16,81 @@ class JSONToNetworkGraph
   # Generate all the nodes
   def genNodes
     @input.each do |i|
-      addupdateNode(@field1, i)
-      addupdateNode(@field2, i)
+      if !(i[@field1].nil? || i[@field2].nil? || i[@field1].empty? || i[@field2].empty?)
+        arrayCheckNode(@field1, i[@field1], i)
+        arrayCheckNode(@field2, i[@field2], i)
+      end
+    end
+  end
+
+  # Handle fields that are arrays
+  def arrayCheckNode(fieldname, fieldvalue, i)
+    if fieldvalue.is_a? Array
+      fieldvalue.each do |a|
+        addupdateNode(fieldname, a, i)
+      end
+    else
+      addupdateNode(fieldname, fieldvalue, i)
     end
   end
 
   # Create or update the appropriate node
-  def addupdateNode(fieldname, i)
-    if !(@nodehash.include? i[fieldname])
-      @nodehash[i[fieldname]] = Node.new(@nodeindex, i[fieldname], fieldname, i)
+  def addupdateNode(fieldname, fieldvalue, i)
+    if !(@nodehash.include? fieldvalue)
+      @nodehash[fieldvalue] = Node.new(@nodeindex, fieldvalue, fieldname, i)
       @nodeindex += 1
     else
-      @nodehash[i[fieldname]].update(i)
+      @nodehash[fieldvalue].update(i)
     end
   end
 
   # Generate all the links
   def genLinks
     @input.each do |i|
-      identifier = i[@field1]+ "-" + i[@field2]
-      if !@linkhash.include? identifier
-        source = @nodehash[i[@field1]].getID
-        target = @nodehash[i[@field2]].getID
-
-        @nodehash[i[@field1]].addLink
-        @nodehash[i[@field2]].addLink
-
-        @linkhash[identifier] = Link.new(source, target, i[@field1], i[@field2], i)
-      else
-        @linkhash[identifier].update(i)
+      if !(i[@field1].nil? || i[@field2].nil? || i[@field1].empty? || i[@field2].empty?)
+        arrayCheckLink(i[@field1], i[@field2], i)
       end
     end
   end
 
+  # Handle fields that are arrays
+  def arrayCheckLink(fieldvalue1, fieldvalue2, i)
+    if (fieldvalue1.is_a? Array) && !(fieldvalue2.is_a? Array)
+      fieldvalue1.each do |a|
+        addupdateLink(a, fieldvalue2, i)
+      end
+    elsif (fieldvalue2.is_a? Array) && !(fieldvalue1.is_a? Array)
+      fieldvalue2.each do |a|
+        addupdateLink(fieldvalue1, a, i)
+      end
+    elsif (fieldvalue1.is_a? Array) && (fieldvalue2.is_a? Array)
+      fieldvalue1.each do |a|
+        fieldvalue2.each do |b|
+          addupateLink(a, b, i)
+        end
+      end
+    else
+      addupdateLink(fieldvalue1, fieldvalue2, i)
+    end
+  end
+
+  # Create or update the appropriate link
+  def addupdateLink(fieldvalue1, fieldvalue2, i)
+    identifier = fieldvalue1 + "-" + fieldvalue2
+    if !@linkhash.include? identifier
+      source = @nodehash[fieldvalue1].getID
+      target = @nodehash[fieldvalue2].getID
+
+      @nodehash[fieldvalue1].addLink
+      @nodehash[fieldvalue2].addLink
+
+      @linkhash[identifier] = Link.new(source, target, fieldvalue1, fieldvalue2, i)
+    else
+      @linkhash[identifier].update(i)
+    end
+  end
+
+  # Count the number of links
   def linkCount
     @linkhash.each_value do |l|
       count1 = @nodehash[l.instance_variable_get(:@field1)].instance_variable_get(:@linkcount)
